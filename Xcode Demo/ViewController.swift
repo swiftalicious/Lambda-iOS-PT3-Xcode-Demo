c6e3377eac78ce06db44d65bb212402dcd425ed6
Jason <jason@kristens-mbp.lan>
2019-09-11 19:39:41 -0700

    Initial Commit

---
+++ Xcode Demo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Xcode Demo
+//
+//  Created by Jason on 9/11/19.
+//  Copyright © 2019 Jason Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Cmd-option-shift: choose where to open a file
+        // command-/: comment/uncomment a line of code
+        // Control-T: Transpose characters
+        // Control-Command-E: Edit all in scope
+        // Control-I: re-indent selection
+        // Command-[]: move indentation left/right
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
